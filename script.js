aea7b6301de7ddb48085db166a92da4654c5d057
Rosemary Chinasa <okorochinasa20@gmail.com>
2022-03-17 12:58:07 +0100

    Another commit

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 const change = document.getElementById("image1");
-
 change.addEventListener("click", (event) => {
-    event.target.style.background = "red";
-    event.target.style.color = "green";
+    event.target.style.background = "#FF3A3A";
+    event.target.style.color = "#FFFFFF";
 });
 
 const change2 = document.getElementById("image2");
 change2.addEventListener("click", (event) => {
-    event.target.style.background = "blue";
-    event.target.style.color = 'yellow';
+    event.target.style.background = "#040341";
+    event.target.style.color = '#F7E301';
 });
 
 const change3 = document.getElementById("image3");
 change3.addEventListener("click", (event) => {
-    event.target.style.background = "black";
-    event.target.style.color = "white";
+    event.target.style.background = "#000000";
+    event.target.style.color = "#FFFFFF";
 });
 
 const change4 = document.getElementById("image4");
 change4.addEventListener("click", (event) => {
-    event.target.style.background = "Grey";
+    event.target.style.background = "#CECECE";
+    event.target.style.color = "#36033F"
 });
 
 const change5 = document.getElementById("image5");
-
 change5.addEventListener("click", (event) => {
-    event.target.style.background = "red";
-    event.target.style.color = "green";
+    event.target.style.background = "#FF5D02";
+    event.target.style.color = "#000000";
 });
 
 const change6 = document.getElementById("image6");
 change6.addEventListener("click", (event) => {
-    event.target.style.background = "blue";
-    event.target.style.color = 'yellow';
+    event.target.style.background = "#036A07";
+    event.target.style.color = '#FCC8ED';
 });
 
 const change7 = document.getElementById("image7");
 change7.addEventListener("click", (event) => {
-    event.target.style.background = "black";
-    event.target.style.color = "white";
+    event.target.style.background = "#040341";
+    event.target.style.color = "#A4FAFF";
 });
 
 const change8 = document.getElementById("image8");
 change8.addEventListener("click", (event) => {
-    event.target.style.background = "Grey";
+    event.target.style.background = "#FFC9CC";
+    event.target.style.color = "#03064F"
 });
